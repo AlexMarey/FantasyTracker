0710162545efc3fb2079b45af71613f3bff80378
AlexMarey <alx.9.mar@gmail.com>
2019-11-24 20:26:12 -0500

    importing league settings

--- FantasyTracker/main.py
+++ FantasyTracker/main.py
@@ -1,27 +1,22 @@
 import os
+import json
 from FantasyProsScraper import FantasyProsScraper
 from Directory import Directory
 
 if __name__ == "__main__":
     # Variables
     rankings = 'rankings'
-    projections = 'projections'
-    # halfPPR = 'half-point-ppr'
-    qb = 'qb'
-    rb = 'rb' 
-    wr = 'wr'
-    flex = 'flex'
-    te = 'te'
-    k = 'k'
-    # Removed flex for the time being
-    positions = [qb, rb, wr, te, flex, k]
-    league_size = 8
-    week = 12
     
     # Import Team/League Settings
+    with open('teams/PalmettoLeague.json') as jsonFile:
+        leagueSettings = json.load(jsonFile)
 
+    week = leagueSettings["currentWeek"]
+    leagueSize = leagueSettings["leagueSize"]
+    positions = leagueSettings["positions"]
+    
     # Class Initialization
-    FpScraper = FantasyProsScraper(league_size, positions, rankings)
+    FpScraper = FantasyProsScraper(leagueSize, positions, rankings)
     Directory = Directory()
 
     # Create Directories
